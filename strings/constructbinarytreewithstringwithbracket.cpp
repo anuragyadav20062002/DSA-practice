169c8b7c2a5916b00b03a291a6c4504ff2b6939a
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-08-26 22:46:24 +0530

    Create constructbinarytreewithstringwithbracket.cpp

---
+++ strings/constructbinarytreewithstringwithbracket.cpp
@@ -0,0 +1,176 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+    
+    public static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+        
+        // Creating array of Strings from input 
+        // String after spliting by space
+        String ip[] = str.split(" ");
+        
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(queue.size()>0 && i < ip.length)
+        {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static Node inputTree(BufferedReader br) throws IOException
+    {
+        return buildTree(br.readLine().trim());
+    }
+    
+    public static void inorder(Node root)
+    {
+        if (root == null)
+           return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            String s; 
+            s = br.readLine();
+            
+            Solution obj = new Solution();
+            Node res = obj.treeFromString(s);
+            
+            Node.inorder(res);
+            System.out.println();
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+   public static Node treeFromString(String s) {
+       Stack<Node> stack = new Stack<>();
+       int r = 0;
+       Node root = null;
+       while(r < s.length()){
+           char ch = s.charAt(r);
+           if(ch == '(')
+               r++;
+           else if(ch == ')'){
+               stack.pop();
+               r++;
+           }else{
+               int j = r;
+               String num = "";
+               while(j < s.length() && s.charAt(j) >='0' && s.charAt(j) <='9'){
+                   num += s.charAt(j);
+                   j++;
+               }
+               r = j;
+               int val = Integer.parseInt(num);
+               Node node = new Node(val);
+               if(stack.isEmpty()){
+                   root = node;
+                   stack.push(node);
+               }else{
+                   Node cur = stack.pop();
+                   if(cur.left == null)
+                       cur.left = node;
+                   else
+                       cur.right = node;
+                   stack.push(cur);
+                   stack.push(node);
+               }
+           }
+       }
+       return root;
+   }
+}
+        
