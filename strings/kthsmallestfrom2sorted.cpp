52b2d40c12d3b664b8b6a23d7cb7f988e07dd44b
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-08-26 18:39:51 +0530

    Create kthsmallestfrom2sorted.cpp

---
+++ strings/kthsmallestfrom2sorted.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int kthElement(int arr1[], int arr2[], int n, int m, int k)
+    {
+          int i=0;
+        int j=0;
+ 
+       
+       int count = 0; 
+       int ans =0;
+       
+        while(i<n && j<m)
+        {
+            if(arr1[i]>arr2[j])
+            {
+               
+                j++;
+                count++;
+                if(count==k)
+                {
+                    ans = arr2[--j];
+                    break;
+                }
+            }
+            else
+            {
+                i++;
+                count++;
+                if(count==k)
+                {
+                    ans = arr1[--i];
+                    break;
+                }
+            }
+        }
+
+        while(i<n)
+        {
+            i++;
+            count++;
+            if(count==k)
+            {
+                ans = arr1[--i];
+                break;
+            }
+        }
+        
+             
+        while(j<m)
+        {
+            j++;
+            count++;
+            if(count==k)
+            {
+                ans = arr2[--j];
+                break;
+            }
+            
+        }
+        
+ 
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+ 
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m,k;
+		cin>>n>>m>>k;
+		int arr1[n],arr2[m];
+		for(int i=0;i<n;i++)
+			cin>>arr1[i];
+		for(int i=0;i<m;i++)
+			cin>>arr2[i];
+		
+		Solution ob;
+        cout << ob.kthElement(arr1, arr2, n, m, k)<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
