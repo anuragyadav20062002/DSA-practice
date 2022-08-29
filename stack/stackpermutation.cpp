f146a46460c69731b5677d12acd2fb99a1ea4f2b
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-08-30 00:03:02 +0530

    Create stackpermutation.cpp

---
+++ stack/stackpermutation.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int isStackPermutation(int N,vector<int> &A,vector<int> &B){
+        stack<int> s;
+        int j=0;
+        for(int i=0;i<N;++i){
+            s.push(A[i]);
+            while(s.size() && s.top()==B[j]){
+                ++j;
+                s.pop();
+            }
+        }
+        return (j==N&&s.empty());
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n),b(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>b[i];
+        }
+        Solution ob;
+        cout<<ob.isStackPermutation(n,a,b)<<endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
