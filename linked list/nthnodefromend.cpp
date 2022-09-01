977c18a68a4abf077f1844c4d36e68017193633c
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2022-09-01 19:58:23 +0530

    Create nthnodefromend.cpp

---
+++ linked list/nthnodefromend.cpp
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+/* Function to get the nth node from the last of a linked list*/
+int getNthFromLast(struct Node* head, int n);
+
+
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n>>k;
+        int firstdata;
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+
+    cout<<getNthFromLast(head, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+//Function to find the data of nth node from the end of a linked list.
+int getNthFromLast(Node *head, int n)
+{
+        Node * temp1=head;
+      int count=0;
+      Node * temp2=head;
+      while(temp1!=NULL){
+          
+          temp1=temp1->next;
+          count++;
+      }
+      if(n>count){
+          return -1;
+      }
+      else{
+       count=count-n+1;
+      
+      while(count!=1){
+          temp2=temp2->next;
+          count--;
+      }
+       return temp2->data;
+      }
+     
+      
+}
+
